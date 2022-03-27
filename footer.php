2ca6c7fb453cafa6d70b0ec7ff8ad1e77cc429bb
heikejizhk666 <88073553+heikejizhk666@users.noreply.github.com>
2022-03-27 20:11:19 +0800

    修复一些bug，新增多种功能

--- footer.php
+++ footer.php
@@ -1,47 +1,47 @@
 <!--吃水不忘挖井人，请勿删除版权，谢谢！-->
-<!--删除版权可能会导致网站Bug，视为放弃一切技术支持！出问题自理！-->
+<!--删除版权可能会影响网站SEO，视为放弃一切技术支持！出问题自理！-->
 <!--让更多人使用主题，作者才有动力继续更新下去-->
-<!--本主题的制作耗时2天。你删除版权，你的良心难道不会痛吗？-->
+<!--你删除版权，你的良心难道不会痛吗？-->
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 
 <link rel="stylesheet" href="<?php $this->options->themeUrl('style.css'); ?>">
-	<div id="d" class="gd_block" style="height: 145px"></div>
-		<div class="gd_copy gd_copy_middle">
-			<b style="text-align: center">友情链接</b>
-			<!--开启友链请去掉下面左右两边的注释-->
-			
-			<p><a href="<?php $this->options->friendurl1() ?>"><?php $this->options->friendname1() ?></a> · <a href="<?php $this->options->friendurl1() ?>"><?php $this->options->friendname2() ?></a> · <a href="<?php $this->options->friendurl1() ?>"><?php $this->options->friendname3() ?></a></p>
-			
+	<div class="gd_block"></div>
+		<div class="gd_box gd_copy_middle">
+			    <b style="text-align: center">友情链接</b>
+			    <?php Links_Plugin::output(); ?>
+		</div>
 			<!--吃水不忘挖井人，请勿删除版权，谢谢！-->
-			<!--删除版权可能会导致网站Bug，视为放弃一切技术支持！出问题自理！-->
+			<!--删除版权可能会影响网站SEO，视为放弃一切技术支持！出问题自理！-->
 			<!--让更多人使用主题，作者才有动力继续更新下去-->
-			<!--本主题的制作耗时2天。你删除版权，你的良心难道不会痛吗？-->
-			<p>本站已运行<span id="momk"></span></p>
-			Copyright &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
-			<?php _e('Theme by <a href="https://www.314669.xyz">HighPress</a>'); ?><br>
-			<img src="https://www.lovestu.com/wp-content/themes/CorePress-Pro/static/img/icp.svg" style="width: 24px;height: 24px;"><a href="http://beian.miit.gov.cn/" target="_blank"><?php $this->options->icp() ?></a>&nbsp;&nbsp;
-			<img src="https://www.lovestu.com/wp-content/themes/CorePress-Pro/static/img/police.svg" style="width: 24px;height: 24px;"><a href="http://beian.gov.cn/" target="_blank"><?php $this->options->police() ?></a>
-			<?php $this->options->footerCode() ?>
-			<script language="javascript">
-				function show_date_time(){
-				window.setTimeout("show_date_time()", 1000);
-				BirthDay=new Date("<?php $this->options->runtime() ?>");//建站日期（月/日/年）
-				today=new Date();
-				timeold=(today.getTime()-BirthDay.getTime());
-				sectimeold=timeold/1000
-				secondsold=Math.floor(sectimeold);
-				msPerDay=24*60*60*1000
-				e_daysold=timeold/msPerDay
-				daysold=Math.floor(e_daysold);
-				e_hrsold=(daysold-e_daysold)*-24;
-				hrsold=Math.floor(e_hrsold);
-				e_minsold=(hrsold-e_hrsold)*-60;
-				minsold=Math.floor((hrsold-e_hrsold)*-60);
-				seconds=Math.floor((minsold-e_minsold)*-60);
-				momk.innerHTML=daysold+"天"+hrsold+"小时"+minsold+"分"+seconds+"秒" ;
-				}
-				show_date_time();
-			</script>
+			<!--你删除版权，你的良心难道不会痛吗？-->
+			<br><br><div class="gd_box gd_copy_middle">
+			    <p>本站已运行<span id="momk"></span></p>
+		    	Copyright &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
+			    <?php _e('Theme by <a href="https://www.314669.xyz">HighPress</a>'); ?><br>
+			    <img src="https://www.lovestu.com/wp-content/themes/CorePress-Pro/static/img/icp.svg" style="width: 24px;height: 24px;"><a href="http://beian.miit.gov.cn/" target="_blank"><?php $this->options->icp() ?></a>&nbsp;&nbsp;
+			    <img src="https://www.lovestu.com/wp-content/themes/CorePress-Pro/static/img/police.svg" style="width: 24px;height: 24px;"><a href="http://beian.gov.cn/" target="_blank"><?php $this->options->police() ?></a>
+		    	<br><?php $this->options->footerCode() ?>
+			    <script language="javascript">
+			    	function show_date_time(){
+			    	window.setTimeout("show_date_time()", 1000);
+			    	BirthDay=new Date("<?php $this->options->runtime() ?>");//建站日期（月/日/年）
+			    	today=new Date();
+			    	timeold=(today.getTime()-BirthDay.getTime());
+				    sectimeold=timeold/1000
+			    	secondsold=Math.floor(sectimeold);
+			    	msPerDay=24*60*60*1000
+			    	e_daysold=timeold/msPerDay
+			    	daysold=Math.floor(e_daysold);
+			    	e_hrsold=(daysold-e_daysold)*-24;
+			    	hrsold=Math.floor(e_hrsold);
+			    	e_minsold=(hrsold-e_hrsold)*-60;
+			    	minsold=Math.floor((hrsold-e_hrsold)*-60);
+			    	seconds=Math.floor((minsold-e_minsold)*-60);
+			    	momk.innerHTML=daysold+"天"+hrsold+"小时"+minsold+"分"+seconds+"秒" ;
+			    	}
+			    	show_date_time();
+			    </script>
+			</div>
 		</div>
 	</div>
 </div><!-- end #body -->
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * 一款简洁、美观、高性能博客主题<br><a href="https://www.314669.xyz/hightech/docs">主题文档</a>&nbsp;&nbsp;&nbsp;<a href="https://www.314669.xyz/highpress">主题下载</a>&nbsp;&nbsp;&nbsp;<a href="https://jq.qq.com/?_wv=1027&k=TvHysSEV">QQ交流群</a>
+ * 一款简洁、美观、高性能博客主题<br><a href="https://www.314669.xyz/highpress.html">主题下载</a>&nbsp;&nbsp;&nbsp;<a href="https://jq.qq.com/?_wv=1027&k=TvHysSEV">QQ交流群</a>
  * @package HighPress
  * @author HighTech
- * @version 1.0
+ * @version 1.0.1
  * @link https://www.314669.xyz/
  */
 
@@ -26,15 +26,21 @@
             </a>
         </div>
         <div class="gd_main">
-            <div class="gd_block">
-                <div class="gd_box gd_post_header gd_copy_middle">
-                    <div>
-                        <!--一言及欢迎语-->
-                        <h2 style="text-align: center"><img src="<?php $this->options->yiyan() ?>"></h2>
-                        <h2 style="text-align: center">欢迎来到<?php $this->options->title(); ?>!</h2>
+            <div class="gd_box gd_copy_middle">
+                <h2><?php echo $this->options->hello(); ?></h2>
+                <h3><?php echo $this->options->motto(); ?></h3>
+            </div>
+                <div class="gd_block">
+                    <div class="gd_box gd_post_header gd_copy_middle">
+                        <div>
+                            <!--一言及幻灯片-->
+                            <div class="gd_img_100">
+                                <img src="<?php $this->options->yyimg() ?>"><br>
+                                <img src="<?php $this->options->slide() ?>">
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
             <div class="gd_block">
                 <!--引入Typecho页面-->
                 <div class="gd_menu">
@@ -49,7 +55,7 @@
                     <div class="gd_block">
                         <a href="<?php $this->permalink() ?>">
                             <div class="gd_box gd_pd gd_c">
-                                <h2><?php $this->title() ?></h2>
+                                <img src="<?php $this->img() ?>" height="100px"><h2><?php $this->title() ?></h2>
                                 <p>作者: <?php $this->author(); ?> 发布日期: <?php $this->date('Y F j'); ?></p>
                                 <p><?php $this->commentsNum('评论 %d'); ?></p>
                             </div>
